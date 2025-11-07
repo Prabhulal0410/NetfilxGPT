6cc8b3a931b78e7109d74f88e44215a50c66b57b
Prabhulal <prabhulal.raghwani410@gmai.com>
2025-11-07 22:03:41 +0530

    Initial project setup

---
+++ netfilxgpt/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='text-amber-500 text-2xl bg-amber-700'>Hello</p>
+    </>
+  )
+}
+
+export default App
