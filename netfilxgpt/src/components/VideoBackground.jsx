81d0d5085a1667fe5b999bb5e7995f1d349c9acf
Prabhulal <prabhulal.raghwani410@gmail.com>
2025-11-18 22:11:22 +0530

    added addTrailervideo action in movieSlice

--- netfilxgpt/src/components/VideoBackground.jsx
+++ netfilxgpt/src/components/VideoBackground.jsx
@@ -1,19 +1,29 @@
 import React, { useEffect } from 'react'
 import { API_OPTIONS } from '../utils/constants'
+import { useDispatch } from 'react-redux'
+import { addTrailerVideo } from '../utils/moviesSlice'
 
 const VideoBackground = ({movieId}) => {
+
+  const dispatch = useDispatch()
 
   const getMovieVideos = async() => {
     const res = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos`,API_OPTIONS)
     const json = await res.json()
     console.log(json.results)
+    const filterTrailer = json.results.filter(movie => movie.type === "Trailer")
+    const trailer = filterTrailer.length == 0 ? filterTrailer[0] : json.results[0]
+    dispatch(addTrailerVideo(trailer))
+    console.log(trailer)
   }
   useEffect(()=>{
     getMovieVideos()
   },[movieId])
 
   return (
-    <div>VideoBackground</div>
+    <div>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/KLZ6kxu-9i0?si=nLaUE_JpREabJfgs" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    </div>
   )
 }
 
