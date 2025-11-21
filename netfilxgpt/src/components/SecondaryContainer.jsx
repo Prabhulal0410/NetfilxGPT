dc1f12da82c35fe47d3765235ecbf2abe6143fbd
Prabhulal <prabhulal.raghwani410@gmail.com>
2025-11-21 22:06:07 +0530

    create slice for other movie list

--- netfilxgpt/src/components/SecondaryContainer.jsx
+++ netfilxgpt/src/components/SecondaryContainer.jsx
@@ -3,14 +3,29 @@ import MovieList from "./MovieList";
 import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
-  const movies= useSelector((store) => store.movies);
+  const movies = useSelector((store) => store.movies);
 
   return (
-    <div className="relative z-30  md:mt-[-120px] space-y-10 px-4">
-      <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
-      <MovieList title="Popular" movies={movies.popularMovies} />
-      <MovieList title="Top Rated" movies={nowPlayingMovies} />
-      <MovieList title="Upcoming" movies={nowPlayingMovies} />
+    <div className="relative z-30 md:mt-[-120px] space-y-10 px-4">
+      <MovieList
+        title="Now Playing"
+        movies={movies.nowPlayingMovies}
+      />
+
+      <MovieList
+        title="Popular"
+        movies={movies.popularMovies}
+      />
+
+      <MovieList
+        title="Top Rated"
+        movies={movies.topRatedMovies}
+      />
+
+      <MovieList
+        title="Upcoming"
+        movies={movies.upcomingMovies}
+      />
     </div>
   );
 };
