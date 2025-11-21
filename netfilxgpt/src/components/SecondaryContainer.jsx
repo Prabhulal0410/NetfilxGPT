fcc05942c8f2681e5eb35400a010911cf67b11b5
Prabhulal <prabhulal.raghwani410@gmail.com>
2025-11-21 22:01:00 +0530

    added hook for popularmovies

--- netfilxgpt/src/components/SecondaryContainer.jsx
+++ netfilxgpt/src/components/SecondaryContainer.jsx
@@ -3,12 +3,12 @@ import MovieList from "./MovieList";
 import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
-  const { nowPlayingMovies } = useSelector((store) => store.movies);
+  const movies= useSelector((store) => store.movies);
 
   return (
     <div className="relative z-30  md:mt-[-120px] space-y-10 px-4">
-      <MovieList title="Now Playing" movies={nowPlayingMovies} />
-      <MovieList title="Popular" movies={nowPlayingMovies} />
+      <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
+      <MovieList title="Popular" movies={movies.popularMovies} />
       <MovieList title="Top Rated" movies={nowPlayingMovies} />
       <MovieList title="Upcoming" movies={nowPlayingMovies} />
     </div>
