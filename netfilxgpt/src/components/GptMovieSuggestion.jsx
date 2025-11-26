c011c179392832b8306fb2d68af54b611630a927
Prabhulal <prabhulal.raghwani410@gmail.com>
2025-11-26 21:58:21 +0530

    completed search functionality

--- netfilxgpt/src/components/GptMovieSuggestion.jsx
+++ netfilxgpt/src/components/GptMovieSuggestion.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const GptMovieSuggestion = () => {
+
+  const {movieResult,movieNames} = useSelector((store)=>store.search)
+
+  if(!movieNames) return null
+
   return (
-    <div>GptMovieSuggestion</div>
+    <>
+      
+    </>
   )
 }
 
