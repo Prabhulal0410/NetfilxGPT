bd0322611c65a2c6e048379f55117a88ff10b1d2
Prabhulal <prabhulal.raghwani410@gmail.com>
2025-11-27 22:13:33 +0530

    added memoization in this hook

--- netfilxgpt/src/hooks/useTrailerVideos.jsx
+++ netfilxgpt/src/hooks/useTrailerVideos.jsx
@@ -1,23 +1,37 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addTrailerVideo } from "../utils/moviesSlice";
 import { API_OPTIONS } from "../utils/constants";
 
 const useTrailerVideos = (movieId) => {
-  //fetch trailer video
   const dispatch = useDispatch();
 
+  // Check if trailer already exists in Redux store
+  const storedTrailer = useSelector(
+    (store) => store.movies.trailerVideo[movieId]
+  );
+
   const getMovieVideos = async () => {
-    const res = await fetch("https://api.themoviedb.org/3/movie/" + movieId + "/videos",API_OPTIONS);
+    const res = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos`,
+      API_OPTIONS
+    );
     const json = await res.json();
 
-    const filterTrailer = json.results.filter((movie) => movie.type === "Trailer");
-    const trailer = filterTrailer.length > 0 ? filterTrailer[0] : json.results[0];
-    dispatch(addTrailerVideo(trailer));
+    const trailerList = json.results.filter(
+      (movie) => movie.type === "Trailer"
+    );
 
+    const trailer =
+      trailerList.length > 0 ? trailerList[0] : json.results[0];
+
+    dispatch(addTrailerVideo({ movieId, trailer }));
   };
+
   useEffect(() => {
-    getMovieVideos();
+    if (!storedTrailer) {
+      getMovieVideos();
+    }
   }, [movieId]);
 };
 
