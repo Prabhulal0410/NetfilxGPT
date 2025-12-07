d354b0757532c9e1f58a3410b86610e03eb9e7af
Prabhulal <prabhulal.raghwani410@gmail.com>
2025-12-07 21:41:14 +0530

    change tmdb token name

--- netfilxgpt/src/components/GptSearchBar.jsx
+++ netfilxgpt/src/components/GptSearchBar.jsx
@@ -21,7 +21,7 @@ const GptSearchBar = () => {
         method: "GET",
         headers: {
           accept: "application/json",
-          Authorization: "Bearer " + import.meta.env.VITE_TMDB_KEY,
+          Authorization: "Bearer " + import.meta.env.VITE_TMDB_TOKEN
         },
       }
     );
