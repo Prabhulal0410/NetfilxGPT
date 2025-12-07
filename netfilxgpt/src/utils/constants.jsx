b2d502657c5ea10bf71cdb0be45da90170228d30
Prabhulal <prabhulal.raghwani410@gmail.com>
2025-12-07 21:30:00 +0530

    add tmdb api key in vercel env varibales for safety

--- netfilxgpt/src/utils/constants.jsx
+++ netfilxgpt/src/utils/constants.jsx
@@ -1,12 +1,13 @@
 export const LOGO = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" 
 
 export const API_OPTIONS = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTQ2NzExZTM2NzI3OThjYTZiOTljMDVmMGIwOTNmYyIsIm5iZiI6MTc2MzIyNjI0MS44NDE5OTk4LCJzdWIiOiI2OTE4YjI4MTE3N2FkN2RkMjc5YzVjYTAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.cXksHvK_gs_5_mSMf4kOVa--kWuqKHLg9NmnsaTmmxA'
+    accept: "application/json",
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`
   }
 };
+
 
 export const IMG_CDN = "https://image.tmdb.org/t/p/w500"
 
