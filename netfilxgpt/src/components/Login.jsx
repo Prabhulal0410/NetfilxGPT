7151a66332972015e4874f97fbf5b73ed17b96bb
Prabhulal <prabhulal.raghwani410@gmail.com>
2025-12-11 22:14:32 +0530

    .

--- netfilxgpt/src/components/Login.jsx
+++ netfilxgpt/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Header from "./Header";
 import { checkValidData } from "../utils/validate";
 import {
