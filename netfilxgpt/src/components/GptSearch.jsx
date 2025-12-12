c5896cc765f47367d4d837dbd35b6603fde387b1
Prabhulal <prabhulal.raghwani410@gmail.com>
2025-12-12 21:58:35 +0530

    added seo keyword

--- netfilxgpt/src/components/GptSearch.jsx
+++ netfilxgpt/src/components/GptSearch.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import GptSearchBar from './GptSearchBar'
+import React from "react";
+import GptSearchBar from "./GptSearchBar";
 
 const GPTsearch = () => {
   return (
-    <div className="w-full min-h-screen">
+    <div
+      className="w-full min-h-screen"
+      aria-label="AI powered movie recommendation search page"
+      role="main"
+    >
+      {/* SEO: Main content for search engine crawlers */}
       <GptSearchBar />
     </div>
   );
-}
+};
 
 export default GPTsearch;
